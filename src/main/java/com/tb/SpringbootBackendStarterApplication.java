3198eef405b1d98b79c99d4c5eafd377b51ab910
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2017-01-27 14:49:02 +0100

    Initial commit

---
+++ src/main/java/com/tb/SpringbootBackendStarterApplication.java
@@ -0,0 +1,12 @@
+package com.tb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendStarterApplication.class, args);
+	}
+}
